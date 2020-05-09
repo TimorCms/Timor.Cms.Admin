595a2c126cdac7c505c033d2a44262f23399544f
Jax <baiyunchen@vip.qq.com>
2020-05-09 22:25:29 +0800

    add category function impl

--- src/pages/category/Index.vue
+++ src/pages/category/Index.vue
@@ -1,42 +1,86 @@
  <template>
   <div>
-    <a-row :gutter="24">
+    <a-row :gutter="24" style>
       <a-col :span="6">
-        <a-input-search style="margin-bottom: 8px" placeholder="Search" @change="onChange" />
-        <a-tree
-          :expanded-keys="expandedKeys"
-          :auto-expand-parent="autoExpandParent"
-          :show-line="true"
-          :tree-data="categories"
-          :replaceFields="{children:'children', title:'name', key:'id' }"
-          @expand="onExpand"
-        >
-          <template slot="title" slot-scope="{ title }">
-            <span v-if="title.indexOf(searchValue) > -1">
-              {{ title.substr(0, title.indexOf(searchValue)) }}
-              <span
-                style="color: #f50"
-              >{{ searchValue }}</span>
-              {{ title.substr(title.indexOf(searchValue) + searchValue.length) }}
-            </span>
-            <span v-else>{{ title }}</span>
-          </template>
-        </a-tree>
+        <a-card title="分类列表" style="height:calc(100vh - 164px)">
+          <a slot="extra" href="#">新增</a>
+          <a-tree
+            :tree-data="categories"
+            :replaceFields="{children:'children', title:'name', key:'id' }"
+            autoExpandParent
+          >
+          </a-tree>
+        </a-card>
       </a-col>
-      <a-col :span="12">col-12</a-col>
+      <a-col :span="12" style="margin-top:50px;">
+        <a-form
+          :form="editModel"
+          :label-col="{ span: 5 }"
+          :wrapper-col="{ span: 12 }"
+          @submit="createOrUpdateCategory"
+        >
+          <a-form-item label="分类名称">
+            <a-input v-model="editModel.name" />
+          </a-form-item>
+          <a-form-item label="上级分类">
+            <a-select v-model="editModel.parentCategoryId" placeholder="无上级分类" allowClear>
+              <a-select-option
+                v-for="category in allCategories"
+                :key="category.id"
+                :value="category.id"
+              >{{ category.name }}</a-select-option>
+            </a-select>
+          </a-form-item>
+          <a-form-item label="排序">
+            <a-input-number id="inputNumber" v-model="editModel.priority" :min="1" :max="1000000" />
+          </a-form-item>
+          <a-form-item label="分类描述">
+            <a-textarea
+              placeholder
+              v-model="editModel.description"
+              :auto-size="{ minRows: 3, maxRows: 6 }"
+            />
+          </a-form-item>
+          <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+            <a-button type="primary" html-type="submit">提交</a-button>
+          </a-form-item>
+        </a-form>
+      </a-col>
     </a-row>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       expandedKeys: [],
       searchValue: "",
       autoExpandParent: true,
-      categories: []
+      allCategories: [],
+      categories: [],
+      editModel: {
+        id: null,
+        name: "",
+        description: "",
+        priority: 99,
+        parentCategoryId: ""
+      },
+      rules: {
+        name: [
+          {
+            required: true,
+            message: "Please input Activity name",
+            trigger: "blur"
+          },
+          {
+            min: 3,
+            max: 5,
+            message: "Length should be 3 to 5",
+            trigger: "blur"
+          }
+        ]
+      }
     };
   },
   methods: {
@@ -48,37 +92,8 @@ export default {
       console.log(e);
     },
     getCategories() {
-       this.$http.get("/api/v1/categories").then(categories => {
-        categories = [
-          {
-            id: "5ea54fc7b3e8870292811f34",
-            name: "顶级分类",
-            description: "分类描述在这里",
-            priority: 0,
-            parentCategoryId: null
-          },
-          {
-            id: "5ea54fc7b3e8870292811f35",
-            name: "顶级分类2",
-            description: "分类描述在这里",
-            priority: 0,
-            parentCategoryId: null
-          },
-          {
-            id: "5ea54fc7b3e8870292811f36",
-            name: "一级子分类",
-            description: "分类描述在这里",
-            priority: 0,
-            parentCategoryId: "5ea54fc7b3e8870292811f34"
-          },
-          {
-            id: "5ea54fc7b3e8870292811f37",
-            name: "二级子分类",
-            description: "分类描述在这里",
-            priority: 0,
-            parentCategoryId: "5ea54fc7b3e8870292811f36"
-          }
-        ];
+      this.$http.get("/api/v1/categories").then(categories => {
+        this.allCategories = Object.assign({}, categories);
 
         this.categories = [];
 
@@ -100,9 +115,47 @@ export default {
           this.poulateSubCategories(subCategories, categories);
         }
       }
+    },
+    createOrUpdateCategory(e) {
+      e.preventDefault();
+      if (this.editModel.id) {
+        this.update();
+      } else {
+        this.create();
+      }
+    },
+    create() {
+      this.$http.post("/api/v1/categories", this.editModel).then(() => {
+        this.$notification.success({
+          message: "新增分类成功",
+          description: ""
+        });
+        this.resetEdit();
+        this.getCategories();
+      });
+    },
+    update() {
+      this.$http.put("/api/v1/categories", this.editModel).then(() => {
+        this.$notification.success({
+          message: "更新分类成功",
+          description: ""
+        });
+        this.resetEdit();
+        this.getCategories();
+      });
+    },
+    resetEdit() {
+      this.editModel = {
+        id: null,
+        name: "",
+        description: "",
+        priority: 99,
+        parentCategoryId: ""
+      };
     }
   },
-  mounted() {
+  mounted() {},
+  created() {
     this.getCategories();
   }
 };
